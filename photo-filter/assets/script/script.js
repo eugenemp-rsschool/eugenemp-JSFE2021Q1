0fc9a84c12e151420078361d0abbb9017632ad2f
eugenemp <phoenixlp91@mail.ru>
2021-04-25 23:46:59 +0300

    feat: implement resetFilter function

--- photo-filter/assets/script/script.js
+++ photo-filter/assets/script/script.js
@@ -1,31 +1,25 @@
-'use strict'
+"use strict"
 
-const filters = document.querySelector('.filters');
-const btnContainer = document.querySelector('.btn-container');
-const btnScrMode = document.querySelector('.openfullscreen');
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+const filters = document.querySelector(".filters");
+const btnContainer = document.querySelector(".btn-container");
+const btnScrMode = document.querySelector(".openfullscreen");
 
-btnScrMode.addEventListener('click', event => {
-  changeScrMode();
-});
+const inputElement = document.querySelector(".btn-load--input");
 
-filters.addEventListener('input', event => {
-  changeFilter(event.target);
-});
+const currentFilterValues = {
+  blur: 0,
+  invert: 0,
+  sepia: 0,
+  saturate: 0,
+  hue: 0
+};
 
-btnContainer.addEventListener('click', event => {
-  let elem = event.target;
+/* ========================================================================= */
 
-  /* if(elem.matches('.btn-reset')) resetFilter();
-  if(elem.matches('.btn-next')) ;
-  if(elem.matches('.btn-load')) ; */
-  if (elem.matches('.btn-save')) saveImage();
-});
-
-
-function drawImage(src) {
+function drawImage(src, filter) {
   let image = new Image();
 
   image.setAttribute('crossOrigin', 'anonymous');
@@ -33,12 +27,21 @@ function drawImage(src) {
   image.onload = function () {
     canvas.width = image.width;
     canvas.height = image.height;
+    ctx.filter = filter;
     ctx.drawImage(image, 0, 0);
+
   };
 }
 
-function saveImage() {
-  //let fileName = ;
+
+
+
+
+
+
+
+function saveImage(fileName) {
+  fileName = fileName || "image";
   let link = document.createElement('a');
 
   link.href = canvas.toDataURL();
@@ -47,16 +50,66 @@ function saveImage() {
   link.delete;
 }
 
+
+
+
+
+
+
+
 function changeFilter(elem) {
   let value = elem.value;
   let output = elem.nextElementSibling;
 
-  output.value = `${value}`;
+  currentFilterValues[elem.name] = value;
+  output.value = `${currentFilterValues[elem.name]}`;
+
+  let filter = `${elem.name}(${currentFilterValues[elem.name]}px)`;
+  drawImage("/photo-filter/assets/img/img.jpg", filter);
+
+  console.log(`Filter: ${elem.name}, value: ${output.value}`);
+  console.log(`${elem.name}(${currentFilterValues[elem.name]}px)`);
 }
+
+
+
+
+
+
+
 
 function resetFilter() {
-
+  const filterList = document.querySelectorAll(".filter");
+  
+  filterList.forEach(input => {
+    input.value = input.dataset.default;
+    changeFilter(input);
+  });
 }
+
+
+
+
+
+
+
+
+function nextPicture(dayTime) {
+  const link = (
+    `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}`
+    );
+
+  let time = new Date();
+
+
+} 
+
+
+
+
+
+
+
 
 function changeScrMode() {
   if (document.fullscreenElement) {
@@ -67,3 +120,27 @@ function changeScrMode() {
     }
   }
 }
+
+
+
+
+/* ========================================================================= */
+
+filters.addEventListener('input', e => {
+  changeFilter(e.target);
+});
+
+btnContainer.addEventListener('click', e => {
+  let elem = e.target;
+
+  if(elem.matches('.btn-reset')) resetFilter();
+  //if(elem.matches('.btn-next')) ;
+  if (elem.matches('.btn-load')) drawImage();
+  if (elem.matches('.btn-save')) saveImage();
+});
+
+btnScrMode.addEventListener('click', changeScrMode);
+
+
+/* ========================================================================= */
+drawImage("/photo-filter/assets/img/img.jpg");

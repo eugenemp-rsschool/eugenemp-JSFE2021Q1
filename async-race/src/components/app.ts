bccaafd6cf44eb7392a778da5da8e63a64b76fba
eugenemp <phoenixlp91@mail.ru>
2021-06-16 20:44:52 +0300

    feat: implement page switching

--- async-race/src/components/app.ts
+++ async-race/src/components/app.ts
@@ -18,12 +18,28 @@ class App {
     this.headElement = new Header().render();
     this.pageWinners = new Winners().render();
     this.pageGarage = new Garage().render();
+
+    this.headElement.addEventListener('click', (e) => this.switchPage(e));
+  }
+
+  switchPage(e: Event): void {
+    const btnGarage = this.headElement.querySelector('.btn__garage');
+    const btnWinners = this.headElement.querySelector('.btn__winners');
+
+    if (e.target === btnGarage) {
+      this.pageWinners.remove();
+      this.appElement.appendChild(this.pageGarage);
+    }
+    if (e.target === btnWinners) {
+      this.pageGarage.remove();
+      this.appElement.appendChild(this.pageWinners);
+    }
   }
 
   render(): HTMLElement {
     [
       this.headElement,
-      this.pageWinners,
+      this.pageGarage,
     ].forEach((elem) => {
       this.appElement.appendChild(elem);
     });

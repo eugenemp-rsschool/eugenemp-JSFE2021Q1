303414338825c35b89789f17af63ef564da88623
eugenemp <phoenixlp91@mail.ru>
2021-06-20 23:39:39 +0300

    feat: implement car start function

--- async-race/src/components/app.ts
+++ async-race/src/components/app.ts
@@ -11,6 +11,7 @@ import {
   updateCar,
   deleteWinner,
   carSwitchEngine,
+  carSwitchToDrive,
 } from './shared/api';
 import './app.scss';
 
@@ -47,7 +48,68 @@ class App {
           this.winners.getWinnersFromServer();
         }
       }
+
+      if ((e.target as HTMLElement).classList.contains('btn__start')) {
+        const carWrapper = (e.target as HTMLElement).closest('.car__wrapper');
+        this.startStopCar(carWrapper as HTMLElement, 'started');
+      }
+
+      if ((e.target as HTMLElement).classList.contains('btn__stop')) {
+        const carWrapper = (e.target as HTMLElement).closest('.car__wrapper');
+        this.startStopCar(carWrapper as HTMLElement, 'stop');
+      }
+
+      if ((e.target as HTMLElement).classList.contains('car-mgmt__btn__race')) {
+        this.startRace();
+      }
+
+      if ((e.target as HTMLElement).classList.contains('car-mgmt__btn__reset')) {
+        this.resetCar();
+      }
     });
+  }
+
+  startStopCar(carWrap: HTMLElement, mode: string): void {
+    const carID = carWrap?.id;
+    const carTrack = carWrap?.querySelector('.car__track');
+    const carElement = carWrap?.querySelector('.car');
+
+    if (carID && mode === 'started') {
+      carSwitchEngine(parseInt(carID, 10), 'started')
+        .then((data) => {
+          const time = Math.round(data.distance / data.velocity);
+          const timeReadable = parseInt((data.distance / data.velocity / 1000).toFixed(2), 10);
+          const distance = (carTrack as HTMLElement).getBoundingClientRect().width;
+          const timeStep = time / 100;
+          const distanceStep = distance / 100;
+
+          let passedDist = distanceStep;
+          let steps = 0;
+
+          const move = setInterval(() => {
+            (carElement as HTMLElement).style.left = `${passedDist}px`;
+            passedDist += distanceStep;
+            steps += 1;
+            if (steps === 90) clearInterval(move);
+          }, timeStep);
+
+          carSwitchToDrive(parseInt(carID, 10))
+            .then((response) => {
+              console.log(response.status);
+              if (response.status === 500) {
+                clearInterval(move);
+              }
+            });
+        });
+    }
+  }
+
+  startRace(): void {
+    const carsWrapper = this.pageGarage.querySelector('.garage__wrapper');
+  }
+
+  resetCar(): void {
+    const carsWrapper = this.pageGarage.querySelector('.garage__wrapper');
   }
 
   switchPage(e: Event): void {

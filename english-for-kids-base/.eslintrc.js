fb562020f2158f0f8d8aa23626ab34af4436fa6a
eugenemp <phoenixlp91@mail.ru>
2021-07-05 05:48:24 +0300

    feat: initial project configuration

---
+++ english-for-kids-base/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "airbnb-typescript/base"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "tsconfigRootDir": __dirname,
+        "project": "./tsconfig.json"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+    }
+};
---
+++ english-for-kids-base/webpack.config.js
@@ -0,0 +1,92 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const isProduction = process.env.NODE_ENV == 'development';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+const config = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext]',
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        contentBase: path.join(__dirname, 'dist'),
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+
+        new HtmlWebpackPlugin({
+            title: 'async-race',
+            template: './src/index.html',
+          }),
+      
+        new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        }),
+      
+        new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false,
+        }),
+    
+        new ESLintPlugin({
+        extensions: ['.js', '.ts'],
+        }),
+        new CopyPlugin({
+        patterns: [
+            { context: './src/assets/', from: 'images/*', to: 'assets', noErrorOnMissing: true },
+            { context: './src/assets/', from: 'icons/*', to: 'assets', noErrorOnMissing: true },
+        ],
+        options: {
+            concurrency: 100,
+        },
+        }),
+    ],
+    module: {
+        // Add your rules for custom modules here
+        // Learn more about loaders from https://webpack.js.org/loaders/
+
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader,'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(ico|eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};

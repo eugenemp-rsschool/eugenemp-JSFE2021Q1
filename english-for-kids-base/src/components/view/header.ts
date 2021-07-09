8d5958ac528aa1606efb8784c4156dfa12f01fad
eugenemp <phoenixlp91@mail.ru>
2021-07-10 02:16:02 +0300

    feat: add score stars container

--- english-for-kids-base/src/components/view/header.ts
+++ english-for-kids-base/src/components/view/header.ts
@@ -3,12 +3,14 @@ import './header.scss';
 
 export default class Header {
   private readonly headerElement: HTMLElement;
+  private readonly headerStarsBox: HTMLElement;
   readonly headerBtnMenu: HTMLElement;
   readonly headerBtnMode: HTMLElement;
 
   constructor() {
     this.headerElement = new Component('header', 'header').render();
     this.headerBtnMenu = new Component('button', 'header__btn__menu').render();
+    this.headerStarsBox = new Component('div', 'header__stars-box').render();
     this.headerBtnMode = new Component('label', 'header__switch__mode').render();
 
     this.headerBtnMode.innerHTML = `<input class="header__switch__mode__input" type="checkbox">
@@ -19,6 +21,7 @@ export default class Header {
   render(): HTMLElement {
     [
       this.headerBtnMenu,
+      this.headerStarsBox,
       this.headerBtnMode,
     ].forEach((btn) => this.headerElement.appendChild(btn));
 

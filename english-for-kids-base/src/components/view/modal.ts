3d79b87453c2d6238bb34980293396ecaa88c357
eugenemp <phoenixlp91@mail.ru>
2021-07-17 05:57:35 +0300

    feat: update  modal component

--- english-for-kids-base/src/components/view/modal.ts
+++ english-for-kids-base/src/components/view/modal.ts
@@ -2,19 +2,32 @@ import Component from './view-component';
 import './modal.scss';
 
 export default class Modal {
+  private readonly modalBackground: HTMLElement;
   private readonly modalElement: HTMLElement;
+  private readonly modalContent: HTMLElement;
 
-  constructor(heading: string, text: string) {
+  constructor(heading: string, content: HTMLElement | string) {
+    this.modalBackground = new Component('div', 'modal__bg').render();
     this.modalElement = new Component('div', 'modal').render();
+    this.modalContent = new Component('div', 'modal__content').render();
+
     this.modalElement.innerHTML = `<div class="modal__header">
                                      <span class="modal__heading">${heading}</span>
-                                   </div>
-                                   <div class="modal__content">
-                                     <span class="modal__text">${text}</span>
                                    </div>`;
+
+    if (content instanceof HTMLElement) {
+      this.modalContent.appendChild(content);
+    }
+
+    if (typeof content === 'string') {
+      this.modalContent.innerHTML = `<span class="modal__text">${content}</span>`;
+    }
   }
 
   render(): HTMLElement {
-    return this.modalElement;
+    this.modalElement.appendChild(this.modalContent);
+    this.modalBackground.appendChild(this.modalElement);
+
+    return this.modalBackground;
   }
 }

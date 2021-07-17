1f5161dabe553cff9af513aeb2a4b77da24ad0ac
eugenemp <phoenixlp91@mail.ru>
2021-07-17 05:57:05 +0300

    feat: add modal content for game cycle

---
+++ english-for-kids-base/src/components/view/modal-content-game.ts
@@ -0,0 +1,5 @@
+import './modal-content-game.scss';
+
+export const MODAL_SUCCESS = '<div class="moda__content__image image-succes"></div>';
+
+export const MODAL_FAILURE = '<div class="moda__content__image image-failure"></div>';

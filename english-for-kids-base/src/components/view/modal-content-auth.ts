85764c10999739439e7b25747b3601fc1dc50132
eugenemp <phoenixlp91@mail.ru>
2021-07-21 23:34:38 +0300

    feat: add auth modal component

---
+++ english-for-kids-base/src/components/view/modal-content-auth.ts
@@ -0,0 +1,7 @@
+import './modal-content';
+
+export const MODAL_AUTH = `<form class="auth__form">
+                             <input class="auth__input input-name"></input>
+                             <input class="auth__input input-pass"></input>
+                             <btn class="auth__btn-submit"></btn>
+                           <form>`;
